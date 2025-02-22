e0674e5a9e0a14acca2edef812ed3d5edf0c480a
shekh15-5569 <shekh15-5569@diu.edu.bd>
2025-02-22 05:27:32 +0000

    cached Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const Lab06AdvancedUI());
 }
 
-class MyApp extends StatelessWidget {
+class Lab06AdvancedUI extends StatelessWidget {
+  const Lab06AdvancedUI({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: "Lab O6 Advanced UI",
       home: Scaffold(
-        appBar: AppBar(title: Text("lab 06")),
-        body: Center( 
-          child: Image.network(
-            'https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg', 
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-            errorBuilder: (context, error, stackTrace) {
-              return Icon(Icons.error, size: 50, color: Colors.red);
-            },
+        appBar: AppBar(title: Text("Lab O6 Advanced UI")),
+        body: Center(
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl:
+                    "https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Image.network(
+                    'https://media.istockphoto.com/id/183412466/photo/eastern-bluebirds-male-and-female.jpg?s=612x612&w=0&k=20&c=6_EQHnGedwdjM9QTUF2c1ce7cC3XtlxvMPpU5HAouhc='),
+              ),
+              Expanded(
+                child: Image(
+                  image: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYyUSpU3kL279ei16K3JgL7h86WKKWHXcJIA&s'),
+                ),
+              )
+            ],
           ),
         ),
       ),
