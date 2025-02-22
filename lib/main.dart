4d8261bc30d38a939adf8621ae122373039fe2be
shekh15-5569 <shekh15-5569@diu.edu.bd>
2025-02-22 06:31:00 +0000

    Drawer Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,9 @@ class Lab06AdvancedUI extends StatelessWidget {
           child: ListView(
             children: [
               DrawerHeader(
-                padding: EdgeInsets.all(8.0),  // Added padding for better alignment
+                padding: EdgeInsets.all(
+                  8.0,
+                ), // Added padding for better alignment
                 child: Row(
                   children: [
                     Icon(Icons.people_alt_rounded, color: Colors.white),
@@ -46,7 +48,8 @@ class Lab06AdvancedUI extends StatelessWidget {
           ),
         ),
         appBar: AppBar(title: Text("Lab O6 Advanced UI")),
-        body: SingleChildScrollView(  // Wrap the body to avoid overflow
+        body: SingleChildScrollView(
+          // Wrap the body to avoid overflow
           child: Center(
             child: Column(
               children: [
@@ -54,17 +57,28 @@ class Lab06AdvancedUI extends StatelessWidget {
                   imageUrl:
                       "https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg",
                   placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => Image.network(
-                    'https://media.istockphoto.com/id/183412466/photo/eastern-bluebirds-male-and-female.jpg?s=612x612&w=0&k=20&c=6_EQHnGedwdjM9QTUF2c1ce7cC3XtlxvMPpU5HAouhc=',
-                  ),
+                  errorWidget:
+                      (context, url, error) => Image.network(
+                        'https://media.istockphoto.com/id/183412466/photo/eastern-bluebirds-male-and-female.jpg?s=612x612&w=0&k=20&c=6_EQHnGedwdjM9QTUF2c1ce7cC3XtlxvMPpU5HAouhc=',
+                      ),
                 ),
-                SizedBox(height: 20),  // Added space between images
+                SizedBox(height: 20), // Added space between images
                 Image.network(
                   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYyUSpU3kL279ei16K3JgL7h86WKKWHXcJIA&s',
                 ),
               ],
             ),
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
         ),
       ),
     );
